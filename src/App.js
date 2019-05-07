1d19690b45e7570205c01d9f872ed52acda0f3b2
Jayme Woogerd <jwoogerd@plaid.com>
2019-05-07 08:17:19 -0700

    Sunrise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
+ /* global Plaid */
+import envvar from 'envvar';
+import plaid from 'plaid';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+
+import './app.css';
+
+const PLAID_CLIENT_ID = envvar.string('REACT_APP_PLAID_CLIENT_ID');
+const PLAID_PUBLIC_KEY = envvar.string('REACT_APP_PLAID_PUBLIC_KEY')
+const PLAID_SECRET = envvar.string('REACT_APP_PLAID_SECRET');
+const PLAID_ENV = envvar.string('REACT_APP_PLAID_ENV');
+
+let PUBLIC_TOKEN;
+let ACCESS_TOKENS = [];
+const ITEM_IDS = [];
+
+const client = new plaid.Client(
+  PLAID_CLIENT_ID,
+  PLAID_SECRET,
+  PLAID_PUBLIC_KEY,
+  plaid.environments[PLAID_ENV],
+  { version: '2018-05-22' }
+);
+
+const plaidLink = Plaid.create({
+  apiVersion: 'v2',
+  clientName: 'Mort Calc',
+  env: PLAID_ENV,
+  product: 'assets',
+  key: PLAID_PUBLIC_KEY,
+  onSuccess: async public_token => {
+    PUBLIC_TOKEN = public_token;
+    try {
+      const { access_token, item_id } = await client.exchangePublicToken(PUBLIC_TOKEN);
+      ACCESS_TOKENS.push(access_token);
+      ITEM_IDS.push(item_id);
+    } catch(err) {
+      console.log(err);
+    }
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Hello</header>
+      <Button variant="primary" onClick={() => plaidLink.open()}>
+        Connect to Plaid
+      </Button>
     </div>
   );
 }
